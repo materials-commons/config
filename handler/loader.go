c1b61bcbf17047ec095dc91509390d0ef547b601
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-05-30 15:08:54 -0400

    Rename KVHandler to Loader. Add comments.

--- handler/loader.go
+++ handler/loader.go
@@ -4,20 +4,20 @@ import (
 	"github.com/materials-commons/config"
 )
 
-type kvHandler struct {
+type loaderHandler struct {
 	values map[string]interface{}
 	loader config.Loader
 }
 
-// KVHandler returns a handler that reads the keys in from a loader.
-func KVHandler(loader config.Loader) config.Handler {
-	return &kvHandler{
+// Loader returns a handler that reads the keys in from a loader.
+func Loader(loader config.Loader) config.Handler {
+	return &loaderHandler{
 		loader: loader,
 	}
 }
 
 // Init loads the keys by calling the loader.
-func (h *kvHandler) Init() error {
+func (h *loaderHandler) Init() error {
 	var vals map[string]interface{}
 	if err := h.loader.Load(&vals); err != nil {
 		return err
@@ -27,14 +27,14 @@ func (h *kvHandler) Init() error {
 }
 
 // Get retrieves keys loaded from the loader.
-func (h *kvHandler) Get(key string) (interface{}, bool) {
+func (h *loaderHandler) Get(key string) (interface{}, bool) {
 	val, found := h.values[key]
 	return val, found
 }
 
 // Set sets the value of keys. You can create new keys, or modify existing ones.
 // Values are not persisted across runs.
-func (h *kvHandler) Set(key string, value interface{}) error {
+func (h *loaderHandler) Set(key string, value interface{}) error {
 	h.values[key] = value
 	return nil
 }
