176ef2f9e6a76f16a7d65935a188f5b64d62c499
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2014-06-01 10:28:02 -0400

    Add new files.

---
+++ handler/viper.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+//	"github.com/materials-commons/config"
+)
+
+const (
+	// Use the default handler
+	Default = "Default"
+
+	// Use the Override handler
+	Override = "Override"
+
+	// Use the environment handler
+	Environment = "Environment"
+
+	// Use the ini handler
+	Ini = "Ini"
+)
---
+++ std.go
@@ -0,0 +1,7 @@
+package config
+
+var std config
+
+func Init() error {
+	return nil
+}
