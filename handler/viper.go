1e718128ec408866b47a4e0b0b098b7a8f7def84
V. Glenn Tarcea <gtarcea@umich.edu>
2014-06-02 14:25:01 -0400

    Create handler that acts like github.com/spf13/viper.

--- handler/viper.go
+++ handler/viper.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-//	"github.com/materials-commons/config"
+	"github.com/materials-commons/config"
 )
 
 const (
@@ -17,3 +17,41 @@ const (
 	// Use the ini handler
 	Ini = "Ini"
 )
+
+// Viper implements github.com/spf13/viper
+func Viper(loader config.Loader) config.Handler {
+	return LowercaseKey(
+		Prioritized(
+			NameHandler(Default, Map()),
+			NameHandler(Override, Map()),
+			NameHandler(Ini, Loader(loader))))
+}
+
+// ViperCaseSensitive implements github.com/spf13/viper except that keys
+// are case sensitive.
+func ViperCaseSensitive(loader config.Loader) config.Handler {
+	return Prioritized(
+		NameHandler(Default, Map()),
+		NameHandler(Override, Map()),
+		NameHandler(Ini, Loader(loader)))
+}
+
+// ViperEx implements github.com/spf13/viper with the addition of environment
+// variables checked before checking for values in the ini file(s).
+func ViperEx(loader config.Loader) config.Handler {
+	return LowercaseKey(
+		Prioritized(
+			NameHandler(Default, Map()),
+			NameHandler(Override, Map()),
+			NameHandler(Environment, Env()),
+			NameHandler(Ini, Loader(loader))))
+}
+
+// ViperExCaseSensitive implements ViperEx except that keys are case sensitive.
+func ViperExCaseSensitive(loader config.Loader) config.Handler {
+	return Prioritized(
+		NameHandler(Default, Map()),
+		NameHandler(Override, Map()),
+		NameHandler(Environment, Env()),
+		NameHandler(Ini, Loader(loader)))
+}
